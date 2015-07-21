24b54ea3a88b44671424b8f733c14598d2aa65de
Sebastian <sebablasko@gmail.com>
2015-07-21 17:16:10 -0300

    added list of number of ports to use

--- run.sh
+++ run.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
 MAX_PACKS=1000000
-repetitions=3
+repetitions=2
 num_port=1820
-total_ports=1
+total_ports_list="1 2 4 8 16 24"
 total_clients=4
 
-for ((i=1 ; $i<=$repetitions ; i++))
-{
-	# Lanzar $total_ports instancias de ServerTesis corriendo en puertos distintos
-	for ((k=0 ; $k<$total_ports ; k++))
-	{
-		./serverTesis --packets $(($MAX_PACKS/$total_ports)) --threads 1 --port $(($num_port+$k)) &
-	}
 
-	sleep 1
+total_sockets="1 2 4 8 16 24 32 48 64"
+for total_ports in $total_ports_list
+do
 
-	for ((j=1 ; $j<=$total_clients ; j++))
+	for ((i=1 ; $i<=$repetitions ; i++))
 	{
+		# Lanzar $total_ports instancias de ServerTesis corriendo en puertos distintos
 		for ((k=0 ; $k<$total_ports ; k++))
 		{
-			./clientTesis --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $(($num_port+$k)) > /dev/null &
+			./serverTesis --packets $(($MAX_PACKS/$total_ports)) --threads 1 --port $(($num_port+$k)) &
 		}
+
+		sleep 1
+
+		for ((j=1 ; $j<=$total_clients ; j++))
+		{
+			for ((k=0 ; $k<$total_ports ; k++))
+			{
+				./clientTesis --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $(($num_port+$k)) > /dev/null &
+			}
+		}
+
+		wait $(pgrep 'serverTesis')
+
+		echo ""
+
 	}
-
-	wait $(pgrep 'serverTesis')
-
-	echo ""
-
-}
+done
