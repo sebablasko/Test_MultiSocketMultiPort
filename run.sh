5cccd2756ef19eb753c3539eb032e817e94a4dfd
Sebastian <sebablasko@gmail.com>
2015-07-22 15:25:57 -0300

    save results in file

--- run.sh
+++ run.sh
@@ -3,19 +3,22 @@
 MAX_PACKS=1000000
 repetitions=5
 num_port=1820
-total_ports_list="5"
+total_ports_list="1 2 4"
 total_clients=4
 client_port_target=13131
+
+salida="times.csv"
 
 for total_ports in $total_ports_list
 do
 
 	for ((i=1 ; $i<=$repetitions ; i++))
 	{
+
 		# Lanzar $total_ports instancias de ServerTesis corriendo en puertos distintos
 		for ((k=0 ; $k<$total_ports ; k++))
 		{
-			./serverTesis --packets $(($MAX_PACKS/$total_ports)) --threads 1 --port $(($num_port+$k)) &
+			./serverTesis --packets $(($MAX_PACKS/$total_ports)) --threads 1 --port $(($num_port+$k)) >> aux &
 		}
 
 		sleep 1
@@ -24,14 +27,16 @@ do
 		{
 			for ((k=0 ; $k<$total_ports ; k++))
 			{
-				#./clientTesis --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $(($num_port+$k)) > /dev/null &
-				./clientTesis --intensive --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $client_port_target > /dev/null &
+				./clientTesis --intensive --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $(($num_port+$k)) > /dev/null &
+				#./clientTesis --intensive --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $client_port_target > /dev/null &
 			}
 		}
 
         wait $(pgrep 'serverTesis')
         kill $(pgrep 'clientTesis')
-
+        cat aux >> $salida
+        rm aux
+        echo "" >> $salida
 		echo ""
 
 	}
