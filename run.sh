a32b6c529be76db937ff356fbd2332868c12d969
Sebastian <sebablasko@gmail.com>
2015-07-28 18:51:14 -0300

    fix

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 MAX_PACKS=1000000
-repetitions=4
+repetitions=20
 num_port=1820
-total_ports_list="1 2 4"
+total_ports_list="1 2 4 8 16 24 32 64 128"
 total_clients=4
 client_port_target=13131
 
@@ -11,19 +11,20 @@ client_port_target=13131
 for total_ports in $total_ports_list
 do
 	echo "Evaluando $total_ports..."
-	ports="1820"
-	for ((k=1 ; $k<$total_ports ; k++))
-	{
-		ports="$ports,$(($num_port+$k))"
-	}
 
 	# Instalar modulo para esta prueba
-	sudo insmod ../UDPRedistributeModule/UDPRedistributeModule.ko verbose=0 _target_hook_port_=13131 _redirect_ports_=$ports
+	sudo insmod ../UDPRedistributeModule/UDPRedistributeModule.ko verbose=0 hook_port=$client_port_target start_redirect_port=$num_port number_redirect_ports=$total_ports
 
 	# Archivo para guardar datos
-	salida="times_$total_ports""_sockets.csv"
+	if (($total_ports >= 0 & $total_ports < 10)); then salida="times_00$total_ports""_sockets.csv"; fi
+	if (($total_ports >= 10 & $total_ports < 100)); then salida="times_0$total_ports""_sockets.csv"; fi
+	if (($total_ports >= 100)); then salida="times_$total_ports""_sockets.csv"; fi
+
+
+
 	for ((i=1 ; $i<=$repetitions ; i++))
 	{
+		echo "Repeticion $i"
 
 		# Lanzar $total_ports instancias de ServerTesis corriendo en puertos distintos partiendo de $num_port
 		for ((k=0 ; $k<$total_ports ; k++))
@@ -35,23 +36,27 @@ do
 
 		for ((j=1 ; $j<=$total_clients ; j++))
 		{
-			for ((k=0 ; $k<$total_ports ; k++))
-			{
+			#for ((k=0 ; $k<$total_ports ; k++))
+			##{
 				#./clientTesis --intensive --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $(($num_port+$k)) > /dev/null &
-				# Le saque el modo intensivo
-				./clientTesis --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $client_port_target > /dev/null &
-			}
+				./clientTesis --intensive --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $client_port_target > /dev/null &
+			#}
 		}
 
         wait $(pgrep 'serverTesis')
-        #kill $(pgrep 'clientTesis')
+        kill $(pgrep 'clientTesis')
         cat aux >> $salida
         rm aux
         echo "" >> $salida
-		echo ""
-
 	}
 
 	sudo rmmod UDPRedistributeModule
 
 done
+
+echo "Compilando resultados"
+
+cat times_* > results.csv
+sed 's/;//g' results.csv | sed 's/,//g' | sed 's/\./,/g' > resultsFix.csv
+
+echo "done"
