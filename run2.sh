f7bef5e8d08d9868f8e3d176d65210c1b254c1f3
Sebastian <sebablasko@gmail.com>
2015-07-29 17:20:56 -0300

    run para probar sched de puertos

---
+++ run2.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+MAX_PACKS=1000000
+repetitions=20
+num_port=1820
+total_ports_list="1 2 4 8 16"
+total_clients=4
+client_port_target=13131
+
+#MODULO sched 1
+for total_ports in $total_ports_list
+do
+	echo "Evaluando $total_ports Usando modulo y random sched..."
+
+	# Instalar modulo para esta prueba
+	sudo insmod ../UDPRedistributeModule/UDPRedistributeModule.ko verbose=0 hook_port=$client_port_target start_redirect_port=$num_port number_redirect_ports=$total_ports port_sched=1
+
+	# Archivo para guardar datos
+	if (($total_ports >= 0 & $total_ports < 10)); then salida="modulo_randomSched_Times_00$total_ports""_sockets.csv"; fi
+	if (($total_ports >= 10 & $total_ports < 100)); then salida="modulo_randomSched_Times_0$total_ports""_sockets.csv"; fi
+	if (($total_ports >= 100)); then salida="modulo_randomSched_Times_$total_ports""_sockets.csv"; fi
+
+	for ((i=1 ; $i<=$repetitions ; i++))
+	{
+		echo "Repeticion $i"
+
+		# Lanzar $total_ports instancias de ServerTesis corriendo en puertos distintos partiendo de $num_port
+		for ((k=0 ; $k<$total_ports ; k++))
+		{
+			./serverTesis --packets $(($MAX_PACKS/$total_ports)) --threads 1 --port $(($num_port+$k)) >> aux &
+		}
+
+		sleep 1
+
+		for ((j=1 ; $j<=$total_clients ; j++))
+		{
+			#for ((k=0 ; $k<$total_ports ; k++))
+			##{
+				#./clientTesis --intensive --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $(($num_port+$k)) > /dev/null &
+				./clientTesis --intensive --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $client_port_target > /dev/null &
+			#}
+		}
+
+        wait $(pgrep 'serverTesis')
+        kill $(pgrep 'clientTesis')
+        cat aux >> $salida
+        rm aux
+        echo "" >> $salida
+	}
+
+	sudo rmmod UDPRedistributeModule
+
+done
+
+echo "Compilando resultados"
+
+cat modulo_randomSched_Times_* > results_randomSched_modulo.csv
+sed 's/;//g' results_randomSched_modulo.csv | sed 's/,//g' | sed 's/\./,/g' > results_randomSched_modulo_FIX.csv
+echo "done"
+
+
+#Modulo sched 2
+for total_ports in $total_ports_list
+do
+	echo "Evaluando $total_ports Usando modulo y sequential sched..."
+
+	# Instalar modulo para esta prueba
+	sudo insmod ../UDPRedistributeModule/UDPRedistributeModule.ko verbose=0 hook_port=$client_port_target start_redirect_port=$num_port number_redirect_ports=$total_ports port_sched=2
+
+	# Archivo para guardar datos
+	if (($total_ports >= 0 & $total_ports < 10)); then salida="modulo_sequentialSched_Times_00$total_ports""_sockets.csv"; fi
+	if (($total_ports >= 10 & $total_ports < 100)); then salida="modulo_sequentialSched_Times_0$total_ports""_sockets.csv"; fi
+	if (($total_ports >= 100)); then salida="modulo_sequentialSched_Times_$total_ports""_sockets.csv"; fi
+
+	for ((i=1 ; $i<=$repetitions ; i++))
+	{
+		echo "Repeticion $i"
+
+		# Lanzar $total_ports instancias de ServerTesis corriendo en puertos distintos partiendo de $num_port
+		for ((k=0 ; $k<$total_ports ; k++))
+		{
+			./serverTesis --packets $(($MAX_PACKS/$total_ports)) --threads 1 --port $(($num_port+$k)) >> aux &
+		}
+
+		sleep 1
+
+		for ((j=1 ; $j<=$total_clients ; j++))
+		{
+			#for ((k=0 ; $k<$total_ports ; k++))
+			##{
+				#./clientTesis --intensive --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $(($num_port+$k)) > /dev/null &
+				./clientTesis --intensive --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $client_port_target > /dev/null &
+			#}
+		}
+
+        wait $(pgrep 'serverTesis')
+        kill $(pgrep 'clientTesis')
+        cat aux >> $salida
+        rm aux
+        echo "" >> $salida
+	}
+
+	sudo rmmod UDPRedistributeModule
+
+done
+
+echo "Compilando resultados"
+
+cat modulo_sequentialSched_Times_* > results_sequentialSched_modulo.csv
+sed 's/;//g' results_sequentialSched_modulo.csv | sed 's/,//g' | sed 's/\./,/g' > results_sequentialSched_modulo_FIX.csv
+echo "done"
