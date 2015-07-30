d0837170cd89403952d7ad9ef9f27903d6bcf15e
Sebastian <sebablasko@gmail.com>
2015-07-30 17:39:50 -0300

    fix

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 MAX_PACKS=1000
-repetitions=2
+repetitions=1
 num_port=1820
-total_ports_list="1 2"
+total_ports_list="1 2 4 8 16 24 36 48 64 128"
 total_clients=4
 client_port_target=13131
 
@@ -190,51 +190,51 @@ echo "done"
 
 
 
-#USANDO PYPROXY
-for total_ports in $total_ports_list
-do
-	echo "Evaluando $total_ports pasando por el proxy en python..."
+# #USANDO PYPROXY
+# for total_ports in $total_ports_list
+# do
+# 	echo "Evaluando $total_ports pasando por el proxy en python..."
 
-	# Ejecutar UDPRedistributePyProxy para distribuir los paquetes
-	python ../UDPRedistributePyProxy/UDPRedistributePyProxy.py --hook_port $client_port_target --start_redirect_port $num_port --number_redirect_ports $total_ports &
-	proxypid=$!
+# 	# Ejecutar UDPRedistributePyProxy para distribuir los paquetes
+# 	python ../UDPRedistributePyProxy/UDPRedistributePyProxy.py --hook_port $client_port_target --start_redirect_port $num_port --number_redirect_ports $total_ports &
+# 	proxypid=$!
 
-	# Archivo para guardar datos
-	if (($total_ports >= 0 & $total_ports < 10)); then salida="pyProxy_Times_00$total_ports""_sockets.csv"; fi
-	if (($total_ports >= 10 & $total_ports < 100)); then salida="pyProxy_Times_0$total_ports""_sockets.csv"; fi
-	if (($total_ports >= 100)); then salida="pyProxy_Times_$total_ports""_sockets.csv"; fi
+# 	# Archivo para guardar datos
+# 	if (($total_ports >= 0 & $total_ports < 10)); then salida="pyProxy_Times_00$total_ports""_sockets.csv"; fi
+# 	if (($total_ports >= 10 & $total_ports < 100)); then salida="pyProxy_Times_0$total_ports""_sockets.csv"; fi
+# 	if (($total_ports >= 100)); then salida="pyProxy_Times_$total_ports""_sockets.csv"; fi
 
-	for ((i=1 ; $i<=$repetitions ; i++))
-	{
-		echo "Repeticion $i"
+# 	for ((i=1 ; $i<=$repetitions ; i++))
+# 	{
+# 		echo "Repeticion $i"
 
-		# Lanzar $total_ports instancias de ServerTesis corriendo en puertos distintos partiendo de $num_port
-		for ((k=0 ; $k<$total_ports ; k++))
-		{
-			./serverTesis --packets $(($MAX_PACKS/$total_ports)) --threads 1 --port $(($num_port+$k)) >> aux &
-		}
+# 		# Lanzar $total_ports instancias de ServerTesis corriendo en puertos distintos partiendo de $num_port
+# 		for ((k=0 ; $k<$total_ports ; k++))
+# 		{
+# 			./serverTesis --packets $(($MAX_PACKS/$total_ports)) --threads 1 --port $(($num_port+$k)) >> aux &
+# 		}
 
-		sleep 1
+# 		sleep 1
 
-		for ((j=1 ; $j<=$total_clients ; j++))
-		{
-			./clientTesis --intensive --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $client_port_target > /dev/null &
-		}
+# 		for ((j=1 ; $j<=$total_clients ; j++))
+# 		{
+# 			./clientTesis --intensive --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $client_port_target > /dev/null &
+# 		}
 
-        wait $(pgrep 'serverTesis')
-        kill $(pgrep 'clientTesis')
-        cat aux >> $salida
-        rm aux
-        echo "" >> $salida
-	}
+#         wait $(pgrep 'serverTesis')
+#         kill $(pgrep 'clientTesis')
+#         cat aux >> $salida
+#         rm aux
+#         echo "" >> $salida
+# 	}
 
-	kill $proxypid
+# 	kill $proxypid
 
-done
+# done
 
-echo "Compilando resultados"
+# echo "Compilando resultados"
 
-cat pyProxy_Times_* > results_pyProxy.csv
-sed 's/;//g' results_pyProxy.csv | sed 's/,//g' | sed 's/\./,/g' > results_pyProxy_FIX.csv
+# cat pyProxy_Times_* > results_pyProxy.csv
+# sed 's/;//g' results_pyProxy.csv | sed 's/,//g' | sed 's/\./,/g' > results_pyProxy_FIX.csv
 
-echo "done"
+# echo "done"
